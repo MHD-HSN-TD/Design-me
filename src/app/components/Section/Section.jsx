6c5039f9431725ee560a5bd8a24091fc7680069b
MHD-HSN-TD <8883597!@mhd>
2024-08-25 13:41:59 +0300

    adjust the padding and the margin for the responsive

--- src/app/components/Section/Section.jsx
+++ src/app/components/Section/Section.jsx
@@ -5,10 +5,10 @@ import Image from "next/image"
 const Section = ({ reverse, imgSrc, text }) => {
     const t = useTranslations(text)
     return (
-        <div className={`flex ${clsx(!reverse && "flex-row", reverse && "flex-row-reverse")} container  m-auto lg:px-20`}>
-            <div className="p-10 basis-1/2"
+        <div className={`lg:flex ${clsx(!reverse && "lg:flex-row", reverse && "lg:flex-row-reverse")} container  m-auto lg:px-20`}>
+            <div className="lg:p-10 p-5 basis-1/2"
                 style={{}}> <Image src={imgSrc} width='500' height='500' alt='' /></div>
-            <div className="basis-1/2 p-10 leading-loose" >
+            <div className="basis-1/2 lg:p-10 p-5 leading-loose" >
                 <h1 className="font-bold text-xl my-7">{t('title')}</h1>{t('description')}</div>
         </div>
     )
